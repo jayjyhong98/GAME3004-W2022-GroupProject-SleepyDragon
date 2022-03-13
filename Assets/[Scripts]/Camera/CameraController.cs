37dabf7399fd009d31b93b203665e0df219abccf
Pauleen Lam <pauleen.lam@outlook.com>
2022-03-12 23:25:43 -0500

    Fix weird cam collision

--- Assets/[Scripts]/Camera/CameraController.cs
+++ Assets/[Scripts]/Camera/CameraController.cs
@@ -22,13 +22,23 @@ public enum CameraPosition
 
 public class CameraController : MonoBehaviour
 {
+    // Player reference
     [SerializeField]
     private GameObject player;
     private PlayerBehaviour playerBehaviour;
 
+    // Camera attributes
     private float cameraRotationSensitivity = 30;
 
+    // Input System reference value
     Vector2 lookVector = Vector2.zero;
+
+    // Camera animation components
+    private bool autoTransitioning = false;
+    private Quaternion startAngle;
+    private Quaternion targetAngle;
+    private float time;
+    private float transitionDuration = 2.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -43,13 +53,29 @@ public class CameraController : MonoBehaviour
         // Update look vecter from player
         lookVector = playerBehaviour.lookVector;
         // Follow the player's position
-        transform.position = player.transform.position;
-        // Rotate the camera based on Vector2 values received from PlayerActionMap
-        transform.rotation *= Quaternion.AngleAxis(lookVector.x * cameraRotationSensitivity * Time.deltaTime, Vector3.up);
-        transform.rotation *= Quaternion.AngleAxis(lookVector.y * cameraRotationSensitivity * Time.deltaTime, Vector3.left);
-        var angle = transform.localEulerAngles;
-        angle.z = 0;
-        transform.localEulerAngles = angle;
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 0.2f, player.transform.position.z);
+
+        if (!autoTransitioning) // If not auto transitioning, let hte player adjust the camera angle
+        {
+            // Rotate the camera based on Vector2 values received from PlayerActionMap
+            transform.rotation *= Quaternion.AngleAxis(lookVector.x * cameraRotationSensitivity * Time.deltaTime, Vector3.up);
+            transform.rotation *= Quaternion.AngleAxis(lookVector.y * cameraRotationSensitivity * Time.deltaTime, Vector3.left);
+            var angle = transform.localEulerAngles;
+            angle.z = 0;
+            transform.localEulerAngles = angle;
+        }
+        else
+        {
+            time += Time.deltaTime;
+            float xRot = Mathf.Lerp(startAngle.x, targetAngle.x, time / transitionDuration); 
+            float yRot = Mathf.Lerp(startAngle.y, targetAngle.y, time / transitionDuration);
+            transform.rotation.Set(xRot, yRot, transform.rotation.z, transform.rotation.w);
+            if (transform.rotation.x == targetAngle.x && transform.rotation.y == targetAngle.y)
+            {
+                autoTransitioning = false;
+                time = 0.0f;
+            }
+        }
     }
 
     // function called by camera adjusting mesh bounds. will change the camera's position/rotation around player
@@ -77,5 +103,10 @@ public class CameraController : MonoBehaviour
         newAngle.z = transform.rotation.z;
 
         transform.rotation = newAngle;
+
+        // For camera animation
+        //targetAngle = newAngle;
+        //autoTransitioning = true;
+        //startAngle = transform.rotation;
     }
 }
